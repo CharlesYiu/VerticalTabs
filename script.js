87bb73e3174468dcbcd2e6cb5159795f55c309c3
Charles Yiu <me@yiu.ch>
2025-01-06 22:20:14 +0800

    Further refined tab handling between VerticalTabs & Chrome, Added Dragging/Moving Tabs, Combined utils.js with script.js, Better logging, General CSS Changes

--- script.js
+++ script.js
@@ -5,18 +5,46 @@ const MAIN_TAB_GROUP_COLLAPSED = true
 const tabsElement = document.getElementById("tabs")
 
 let mainTabGroupId = null
-let activatedTabId = null
 
+let activatedTab = {
+    id: null,
+    index: null
+}
+
+async function getCurrentTab() {
+    return (await chrome.tabs.query({ active: true, lastFocusedWindow: true }))[0]
+}
+async function getAllTabs() {
+    return await chrome.tabs.query({ currentWindow: true })
+}
+
+async function addToMainTabGroup(tabIds) {
+    // try {
+        await chrome.tabs.group({
+            groupId: mainTabGroupId,
+            tabIds: tabIds
+        })
+    // } catch { }
+}
+async function removeFromMainTabGroup(tabIds) {
+    try {
+        await chrome.tabs.ungroup(tabIds)
+    } catch { }
+}
 async function ensureMainTabGroup(){
     let createdNewGroup = false
+
     async function createMainTabGroup() {
         mainTabGroupId = await chrome.tabs.group({
             tabIds: (await getAllTabs()).map(tab => tab.id)
         })
-        await activateTab(activatedTabId)
+        await activateTab(activatedTab.id)
 
         createdNewGroup = true
+
+        console.log("G", mainTabGroupId, "Main Tab Group Created")
     }
+
     if (mainTabGroupId == null) {
         let groups = await chrome.tabGroups.query({
             title: MAIN_TAB_GROUP_TITLE,
@@ -25,15 +53,16 @@ async function ensureMainTabGroup(){
         })
         if (groups.length > 0) {
             mainTabGroupId = groups[0].id
+
+            console.log("G", mainTabGroupId, "Main Tab Group Found")
             return
         }
         await createMainTabGroup()
     } else {
-        try { chrome.tabGroups.get(mainTabGroupId) == null }
-        catch {
-            await createMainTabGroup()
-        }
+        try { await chrome.tabGroups.get(mainTabGroupId) }
+        catch { await createMainTabGroup() }
     }
+
     await chrome.tabGroups.update(mainTabGroupId, {
         title: MAIN_TAB_GROUP_TITLE,
         color: MAIN_TAB_GROUP_COLOR,
@@ -41,78 +70,134 @@ async function ensureMainTabGroup(){
     })
     return createdNewGroup
 }
+async function gatherToMainTabGroup() {
+     await chrome.tabs.group({
+         groupId: mainTabGroupId,
+         tabIds: (await getAllTabs()).map(tab => tab.id).filter(tabId => tabId !== activatedTab.id)
+    })
+}
 
 chrome.tabs.onActivated.addListener(activeInfo => {
-    ensureMainTabGroup()
-        .then(async function(skip) {
-            if (skip) return
-            console.log(activatedTabId, activeInfo.tabId)
-            const previousTabElement = await ensureTabElement(activatedTabId)
+    chrome.windows.getCurrent({ })
+        .then(async function(currentWindow) {
+            if (currentWindow.id !== activeInfo.windowId) return
+            if (await ensureMainTabGroup()) return
+            const previousTabElement = await ensureTabElement(activatedTab.id)
             const tabElement = await ensureTabElement(activeInfo.tabId)
-            console.log(activeInfo)
-            if (activatedTabId != null) {
-                try {
-                    await chrome.tabs.group({
-                        groupId: mainTabGroupId,
-                        tabIds: [activatedTabId]
-                    })
-                } catch { }
+
+            const listIndex = Array.prototype.indexOf.call(tabsElement.children, document.getElementById(activeInfo.tabId).parentElement)
+            const activatingTabIndex = listIndex === -1 ? (await chrome.tabs.get(activeInfo.tabId)).index : listIndex
+
+            if (activatedTab.id != null) {
+                const lastTabIndex = tabsElement.children.length - 1
+                await addToMainTabGroup([activatedTab.id])
+                await chrome.tabs.move(activatedTab.id, {
+                    index: activatedTab.index -
+                        ((activatedTab.index >= lastTabIndex) ||
+                        (activatedTab.index > activatingTabIndex) ? 1 : 0)
+                })
+
             }
-            await chrome.tabs.ungroup([activeInfo.tabId])
-            activatedTabId = activeInfo.tabId
-            previousTabElement.classList.remove("active")
+
+            activatedTab = {
+                id: activeInfo.tabId,
+                index: activatingTabIndex
+            }
+
+            await removeFromMainTabGroup([activeInfo.tabId])
+
+            if (previousTabElement != null) previousTabElement.classList.remove("active")
             tabElement.classList.add('active')
+
             await chrome.tabGroups.update(mainTabGroupId, {
                 collapsed: MAIN_TAB_GROUP_COLLAPSED
             })
+
+            console.log(activeInfo.tabId, "Tab Activation Heard")
+        })
+})
+chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
+    chrome.windows.getCurrent({ })
+        .then(async function(currentWindow) {
+            if (currentWindow.id !== removeInfo.windowId) return
+
+            const tabElement = document.getElementById(tabId)
+            if (tabElement == null) return
+
+            tabElement.parentElement.remove()
         })
 })
 chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
     const tabElement = document.getElementById(tabId)
     if (tabElement == null) return
-    console.log("changed")
-    if (changeInfo.title != null) tabElement.children[3].textContent = changeInfo.title
-    if (changeInfo.favIconUrl != null) tabElement.children[2].src = changeInfo.favIconUrl
+    if (changeInfo.title != null) {
+        tabElement.children[3].textContent = changeInfo.title
+        console.log(tabId, "Title Changed", changeInfo.title)
+    }
+    if (changeInfo.favIconUrl != null) {
+        tabElement.children[2].src = changeInfo.favIconUrl
+        console.log(tabId, "FavIcon Changed", changeInfo.favIconUrl)
+    }
     switch (changeInfo.status) {
         case "unloaded":
             tabElement.classList.add("unloaded")
+            console.log(tabId, "Tab Unloaded")
             break
         case "loading":
             tabElement.classList.remove("unloaded")
             tabElement.classList.add("loading")
+            console.log(tabId, "Tab Loading")
             break
         case "complete":
             tabElement.classList.remove("loading", "unloaded")
+            console.log(tabId, "Tab Complete")
             break
         default:
             break
     }
+
+    console.log(tabId, "Tab Update Heard")
 })
 
+async function activateTab(tabId) {
+    await ensureMainTabGroup()
+    await chrome.tabs.update(tabId, { active: true })
+    await chrome.tabs.move(tabId, { index: -1 })
+
+    console.log(tabId, "Activated Tab Programmatically")
+}
 async function removeTab(tabId) {
     await ensureMainTabGroup()
     await chrome.tabs.remove(tabId)
-    activatedTabId = (await getCurrentTab()).id
-    console.log(tabId, activatedTabId)
-    await activateTab(activatedTabId)
+    activatedTab = {
+        id: null,
+        index: null
+    }
+
+    console.log(tabId, "Removed Tab Programmatically")
 }
-async function activateTab(tabId) {
-    await ensureMainTabGroup()
-    await chrome.tabs.ungroup([tabId])
-    await chrome.tabs.update(tabId, {
-        active: true
-    })
-    await chrome.tabs.move(tabId, {
-        index: -1
-    })
+
+function adjustActivatedTabIndex(tabId, newIndex, offset) {
+    if (activatedTab.id === tabId) activatedTab.index = newIndex
+    else if (activatedTab.index > newIndex) activatedTab.index -= 1
+    else if (activatedTab.index === newIndex) {
+        if (offset > 0) {
+            activatedTab.index -= 1
+        } else if (offset < 0) {
+            activatedTab.index += 1
+        }
+    }
 }
 
 async function addTabElement(tab) {
-    console.log("added tab", tab)
+    const tabContainer = document.createElement("div")
+    tabContainer.className = "tab-container"
 
     const tabElement = document.createElement("div")
     tabElement.className = `tab${tab.active ? " active" : ""}`
+    tabElement.draggable = true
     tabElement.id = tab.id
+    tabContainer.appendChild(tabElement)
 
     const tabCloseButton = document.createElement("button")
     tabCloseButton.innerHTML = `<img src="assets/close.png" draggable="false">`
@@ -131,39 +216,72 @@ async function addTabElement(tab) {
     tabElement.appendChild(tabTitleElement)
 
     tabElement.addEventListener("click",  () => {
-        const previousTabElement = document.getElementById(activatedTabId)
+        const previousTabElement = document.getElementById(activatedTab.id)
         if (previousTabElement != null) previousTabElement.classList.remove("active")
-        console.log(previousTabElement, tab.id, activatedTabId)
         activateTab(tab.id)
     })
     tabCloseButton.addEventListener("click", event => {
         removeTab(tab.id)
-        tabElement.remove()
         event.stopPropagation()
     })
 
-    tabsElement.appendChild(tabElement)
+    tabContainer.addEventListener("dragstart", _ => {
+        const tabContainerIndex = Array.prototype.indexOf.call(tabsElement.children, tabContainer)
+        dragged = {
+            target: tabContainer,
+            startIndex: tabContainerIndex
+        }
+        tabElement.classList.add("dragging")
+    })
+    tabContainer.addEventListener("dragend", _ => {
+        tabElement.classList.remove("dragging")
+    })
+    tabContainer.addEventListener("dragover", event => {
+        event.preventDefault()
+        if (tabContainer === dragged.target) return
+
+        const tabContainerIndex = Array.prototype.indexOf.call(tabsElement.children, tabContainer)
+        const draggedContainerIndex = Array.prototype.indexOf.call(tabsElement.children, dragged.target)
+        const offsetIndex = draggedContainerIndex <= tabContainerIndex ? 1 : 0
+
+        tabsElement.insertBefore(dragged.target, tabsElement.children[tabContainerIndex + offsetIndex])
+    })
+    tabContainer.addEventListener("drop", _ => {
+        const tabContainerIndex = Array.prototype.indexOf.call(tabsElement.children, tabContainer)
+        const offset = (tabContainerIndex - dragged.startIndex)
+        if (tab.id !== activatedTab.id) chrome.tabs.move(tab.id, {
+            index: tabContainerIndex - (activatedTab.index < tabContainerIndex ? 1 : 0)
+        })
+        adjustActivatedTabIndex(tab.id, tabContainerIndex, offset)
+        console.log(tab.id, "Tab Move Ended", offset)
+    })
+
+    tabsElement.appendChild(tabContainer)
+
+    console.log(tab.id, "Added Tab Element", tabContainer)
     return tabElement
 }
 async function ensureTabElement(tabId) {
     let tabElement = document.getElementById(tabId)
     if (tabElement == null) {
-        const tab = await chrome.tabs.get(tabId)
-        tabElement = await addTabElement(tab)
+        try {
+            const tab = await chrome.tabs.get(tabId)
+            tabElement = await addTabElement(tab)
+        } catch { return null }
     }
     return tabElement
 }
 
-document.getElementById("newtab").addEventListener("click", _ => {
-    chrome.tabs.create({})
-})
-
 function processTabs() {
     getCurrentTab()
         .then(async function (currentTab) {
-            activatedTabId = currentTab.id
+            activatedTab = {
+                id: currentTab.id,
+                index: currentTab.index
+            }
 
             await ensureMainTabGroup()
+            await gatherToMainTabGroup()
             const tabs = await getAllTabs()
             tabs.forEach(async function(tab) {
                 await addTabElement(tab)
@@ -173,9 +291,7 @@ function processTabs() {
 processTabs()
 
 document.getElementById("viewsource").addEventListener("click", _ => {
-    chrome.tabs.create({
-        url: "https://github.com/charlesyiu/verticaltabs"
-    })
+    chrome.tabs.create({ url: "https://github.com/charlesyiu/verticaltabs" })
 })
 document.getElementById("reprocess").addEventListener("click", _ => {
     mainTabGroupId = null
@@ -189,4 +305,8 @@ window.addEventListener("click", _ => {
 document.getElementById('menu').addEventListener('click', event => {
     menubar.classList.toggle("hide")
     event.stopPropagation()
+})
+
+document.getElementById("newtab").addEventListener("click", _ => {
+    chrome.tabs.create({})
 })
--- utils.js
+++
@@ -1,9 +0,0 @@
-function getFaviconUrl(url) {
-    return `chrome-extension://${chrome.runtime.id}/_favicon/?pageUrl=${encodeURIComponent(url)}&size=32`;
-}
-async function getCurrentTab() {
-    return (await chrome.tabs.query({ active: true, lastFocusedWindow: true }))[0]
-}
-async function getAllTabs() {
-    return await chrome.tabs.query({ currentWindow: true })
-}
